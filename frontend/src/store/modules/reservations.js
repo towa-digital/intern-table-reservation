1250d605304060bce20baf2cd54ff8a97231a7f9
niklas-mischi <niklas.mischi@gmail.com>
2019-08-16 09:15:56 +0200

    merge conflict

--- frontend/src/store/modules/reservations.js
+++ frontend/src/store/modules/reservations.js
@@ -18,11 +18,7 @@ const getters = {
 
 const actions = {
   addReservation: ({ commit }, reservation) => {
-<<<<<<< HEAD
-    const response = axios
-=======
     axios
->>>>>>> db1e8d5f248167895f6293b61b9672f31920a6d3
       .post(
         "http://localhost/wordpress/wp-json/tischverwaltung/v1/savenewreservation",
         {
@@ -35,33 +31,14 @@ const actions = {
           numberOfSeats: reservation.reservation.numberOfSeats
         }
       )
-<<<<<<< HEAD
-      .then((response) => {
-        // this.submitted = true;
-=======
+
       .then(() => {
->>>>>>> db1e8d5f248167895f6293b61b9672f31920a6d3
         commit("reservationAccepted")
 
       })
       .catch(error => {
-<<<<<<< HEAD
-        //   if (error.response) this.errormessage = error.response.data.message;
-        //   this.error = true;
-
-        //   console.log(error)
-        commit('reservationDenied', error.response.data.message)
-
-        console.log(error.response)
-      })
-
-
-
-
-=======
         commit('reservationDenied', error.response.data.message)
       })
->>>>>>> db1e8d5f248167895f6293b61b9672f31920a6d3
   }
 }
 
