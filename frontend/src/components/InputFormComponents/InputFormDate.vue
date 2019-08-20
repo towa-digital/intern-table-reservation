8fe14e308556d1d0c0c9abb05eca2346b5bda2a3
niklas-mischi <niklas.mischi@gmail.com>
2019-08-20 09:06:47 +0200

    daily commit

--- frontend/src/components/InputFormComponents/InputFormDate.vue
+++ frontend/src/components/InputFormComponents/InputFormDate.vue
@@ -1,18 +1,33 @@
 <template>
   <form>
     <input
-      id="dateInput"
+      id="today"
       ref="date"
       type="date"
       name="date"
-      placeholder="Datum"
+      
       @input="updateDate"
       v-on:input="emitToParent"
     />
     <select name="time" ref="time" @change="emitToParent">
-      <option disabled selected value v-if="timeSlotsForCurrentWeekday.length != 0">Bitte Uhrzeit wählen.</option>
-            <option disabled selected value v-if="dateFieldValue == '' && timeSlotsForCurrentWeekday.length == 0">Bitte erst Datum wählen.</option>
-      <option disabled selected value v-if="dateFieldValue != '' && timeSlotsForCurrentWeekday.length == 0">nicht geöffnet</option>
+      <option
+        disabled
+        selected
+        value
+        v-if="timeSlotsForCurrentWeekday.length != 0"
+      >Bitte Uhrzeit wählen.</option>
+      <option
+        disabled
+        selected
+        value
+        v-if="dateFieldValue == '' && timeSlotsForCurrentWeekday.length == 0"
+      >Bitte erst Datum wählen.</option>
+      <option
+        disabled
+        selected
+        value
+        v-if="dateFieldValue != '' && timeSlotsForCurrentWeekday.length == 0"
+      >nicht geöffnet</option>
       <option v-for="t in timeSlotsForCurrentWeekday" v-bind:key="t.display">{{t.display}}</option>
     </select>
   </form>
@@ -25,37 +40,39 @@ export default {
   data() {
     return {
       timeSlotsForCurrentWeekday: [],
-      dateFieldValue: ""
+      dateFieldValue: "",
+      today: '',
+      currentDate: ''
     };
   },
   methods: {
     emitToParent(event) {
-        var date = this.$refs.date.value;
-        var time = this.$refs.time.value;
-        this.$emit("input", (date === "" || time === "" ? "" : date+"T"+time));
+      var date = this.$refs.date.value;
+      var time = this.$refs.time.value;
+      this.$emit("input", date === "" || time === "" ? "" : date + "T" + time);
 
-        event.preventDefault();
+      event.preventDefault();
     },
     updateDate(event) {
       const val = this.$refs.date.value;
       this.dateFieldValue = val;
 
-      if(this.$refs.date.value === "") {
+      if (this.$refs.date.value === "") {
         this.timeSlotsForCurrentWeekday = [];
-
       } else {
         const weekdays = [6, 0, 1, 2, 3, 4, 5];
         var index = weekdays[new Date(val).getDay()];
         this.timeSlotsForCurrentWeekday = this.$store.getters.timeSlots[index];
-
       }
 
       event.preventDefault();
-
-  },
+    }
   },
   created: function() {
     this.$store.dispatch("getTimeSlots");
+
+    this.currentDate = new Date().toJSON().slice(0,10);
+    document.getElementById("today").defaultValue = this.currentDate;
   }
 };
 </script>
