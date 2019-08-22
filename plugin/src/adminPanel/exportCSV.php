e7722dd7bd5d9e5df0186b025d4a7edf0c917713
DasElias <elias.horner@me.com>
2019-08-22 16:33:03 +0200

    exporting csv doesn't require a file on the web server

--- plugin/src/adminPanel/exportCSV.php
+++ plugin/src/adminPanel/exportCSV.php
@@ -2,36 +2,39 @@
 require_once(__DIR__."/../queryDatabase.php");
 require_once(__DIR__."/../csv.php");
 
+if($_GET["page"] == "exportcsv" && isset($_POST["isClicked"])) {
+    add_action('init', 'downloadCsv');
+    function downloadCsv() {
+        $str = getCsvAsString();
+
+        header("Pragma: public");
+        header("Expires: 0");
+        header("Cache-Control: must-revalidate, post-check=0, pre-check=0"); 
+        header('Content-Type: text/plain'); # Don't use application/force-download - it's not a real MIME type, and the Content-Disposition header is sufficient
+        header('Content-Length: ' . strlen($str));
+        header("Content-Disposition: attachment;filename=reservations.csv ");
+        header('Connection: close');
+        
+        echo $str;
+    }
+}
 
 
 function applyStyle_exportCSV()
 {
-    wp_enqueue_style("export_style", plugins_url("style/export.css", __FILE__));
     wp_enqueue_style("main_style", plugins_url("style/main.css", __FILE__));
-}
+    wp_enqueue_style("export_style", plugins_url("style/export.css", __FILE__));
 
+}
 
 function show_exportCSV()
 {
-    if (isset($_POST["isClicked"])) {
-        exportCSV();
-    }
+?>
 
-    echo __DIR__ .'/../csv/daten.csv'; ?>
+<h1>Reservierungsliste als CSV exportieren</h1>
+<input name="isClicked" type="submit" class="btn" value="neue Liste erstellen"  />
 
-<table>
-    <tr><td>
-        <h3 class="inline">Exportieren Sie Ihre CSV</h3>                     
-    </td></tr>
-    <tr><td>
-        <form method="post">
-            <input name="isClicked" type="submit" class="btn" value="neue Liste erstellen"  />
-        </form>
-    </td></tr>
-    <tr><td>
-        <h4><a href="<?php echo __DIR__ .'\..\csv\daten.csv' ?>" download="Reservationsliste">Ihr Downloadlink</a></h4>
-    </td></tr>
-</table>
+
 
 <?php
 }
