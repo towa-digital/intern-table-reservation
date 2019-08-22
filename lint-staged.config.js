6c066599f68965c4788561ec873a53d3a39d9943
niklas-mischi <niklas.mischi@gmail.com>
2019-08-22 10:31:41 +0200

    husky pre commit hook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // '*.php': ['./vendor/bin/php-cs-fixer fix --config=./.php_cs --allow-risky=yes', 'git add'],
+  '*.php': ['/php-cs-fixer fix --config=./.php_cs --allow-risky=yes', 'git add'],
   '*.md': ['yarn lint:markdownlint', 'yarn lint:prettier', 'git add'],
 };
