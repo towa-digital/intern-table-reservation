2184cac99254d327102ce562c5af2e0fb9b657ae
DasElias <elias.horner@me.com>
2019-08-26 06:33:35 +0200

    new export system

--- plugin/src/adminPanel/exportCSV.php
+++ plugin/src/adminPanel/exportCSV.php
@@ -2,7 +2,7 @@
 require_once(__DIR__."/../queryDatabase.php");
 require_once(__DIR__."/../csv.php");
 
-if($_GET["page"] == "exportcsv" && isset($_POST["isClicked"])) {
+if($_GET["page"] == "exportreservations" && isset($_POST["isClicked"])) {
     add_action('init', 'downloadCsv');
     function downloadCsv() {
         $str = getCsvAsString();
@@ -10,7 +10,7 @@ if($_GET["page"] == "exportcsv" && isset($_POST["isClicked"])) {
         header("Pragma: public");
         header("Expires: 0");
         header("Cache-Control: must-revalidate, post-check=0, pre-check=0"); 
-        header('Content-Type: text/plain'); # Don't use application/force-download - it's not a real MIME type, and the Content-Disposition header is sufficient
+        header('Content-Type: text/plain');
         header('Content-Length: ' . strlen($str));
         header("Content-Disposition: attachment;filename=reservations.csv ");
         header('Connection: close');
@@ -20,20 +20,21 @@ if($_GET["page"] == "exportcsv" && isset($_POST["isClicked"])) {
 }
 
 
-function applyStyle_exportCSV()
+function applyStyle_export()
 {
     wp_enqueue_style("main_style", plugins_url("style/main.css", __FILE__));
     wp_enqueue_style("export_style", plugins_url("style/export.css", __FILE__));
 
 }
 
-function show_exportCSV()
+function show_export()
 {
 ?>
 
 <h1>Reservierungsliste als CSV exportieren</h1>
-<input name="isClicked" type="submit" class="btn" value="neue Liste erstellen"  />
-
+<form method="post">
+    <input name="isClicked" type="submit" class="btn" value="neue Liste erstellen"  />
+</form>
 
 
 <?php
