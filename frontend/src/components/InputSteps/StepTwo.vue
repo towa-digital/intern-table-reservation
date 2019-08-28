7136aafd8c028fe8fe7bb47c604351be8d300966
niklas-mischi <niklas.mischi@gmail.com>
2019-08-28 09:59:45 +0200

    algorythm

--- frontend/src/components/InputSteps/StepTwo.vue
+++ frontend/src/components/InputSteps/StepTwo.vue
@@ -1,24 +1,21 @@
 <template>
   <table>
     <tr v-if="errormessage != ''">
-        <td colspan="2">
-          <div class="centered">
-            <h3 v-html="errormessage"></h3>
-          </div>
-        </td>
-      </tr>
+      <td colspan="2">
+        <div class="centered">
+          <h3 v-html="errormessage"></h3>
+        </div>
+      </td>
+    </tr>
     <tr v-for="t in getCombinations" :key="t.t1">
       <td v-html="getTextForCombination(t)"></td>
-      <td><button type="button" class="btn" @click="onClick(t)">Übernehmen</button></td>
+      <td>
+        <button type="button" class="btn" @click="onClick(t)">Übernehmen</button>
+      </td>
     </tr>
     <tr>
       <td colspan="2">
-        <input
-            type="submit"
-            value="Zurück"
-            class="btn"
-            v-on:click="onBack"
-          />
+        <input type="submit" value="Zurück" class="btn" v-on:click="onBack" />
       </td>
     </tr>
   </table>
@@ -27,14 +24,11 @@
 <script>
 import { mapGetters } from 'vuex';
 
-
 export default {
   name: 'StepTwo',
-  components: {
-  },
+  components: {},
   data() {
-    return {
-    };
+    return {};
   },
   methods: {
     onBack() {
@@ -44,20 +38,20 @@ export default {
     getTextForCombination(t) {
       var seatsMap = new Map();
 
-      for(var singleTable of t) {
-        if(seatsMap.has(singleTable)) {
-          var old = parseInt(seatsMap.get(singleTable))
+      for (var singleTable of t) {
+        if (seatsMap.has(singleTable)) {
+          var old = parseInt(seatsMap.get(singleTable));
           seatsMap.set(singleTable, old++);
         } else {
           seatsMap.set(singleTable, 1);
         }
       }
 
-      var toReturn = "<ul>";
-      for(var entry of seatsMap) {
-        if(entry[0] != undefined) toReturn += "<li>"+entry[1]+" x "+entry[0].seats+" Plätze</li>";
+      var toReturn = '<ul>';
+      for (var entry of seatsMap) {
+        if (entry[0] != undefined) toReturn += '<li>' + entry[1] + ' x ' + entry[0].seats + ' Plätze</li>';
       }
-      toReturn += "</ul>";
+      toReturn += '</ul>';
 
       return toReturn;
     },
@@ -80,61 +74,76 @@ export default {
       } else if (tooMuchTablesForPersons_error) {
         this.$store.commit('setError', 'Du hast zu viele Tische ausgewählt!');
       } else {
-            for(var n of selectedTables) {
-              this.$store.commit("claimTable", n);
-            }
+        for (var n of selectedTables) {
+          this.$store.commit('claimTable', n);
+        }
 
-            this.$store.commit('incrementStepCounter');
-            this.$store.commit('setError', '');
-          }
+        this.$store.commit('incrementStepCounter');
+        this.$store.commit('setError', '');
       }
+    },
   },
   computed: {
-    ...mapGetters(['allTables', "errormessage"]),
+    ...mapGetters(['allTables', 'errormessage']),
     numberOfSeats() {
       return this.$store.getters.StepOne.numberOfSeats;
     },
     getCombinations() {
       // prüfe, ob ein Wert genau passt
       var suitableTables = [];
-      for(var t of this.allTables) {
-        if(t.seats >= this.numberOfSeats && t.isFree && !t.isDisabled) suitableTables.push(t);
+      for (var t of this.allTables) {
+        if (t.seats >= this.numberOfSeats && t.isFree && !t.isDisabled) suitableTables.push(t);
       }
 
-      if(suitableTables.length != 0) {
+      if (suitableTables.length != 0) {
         suitableTables.sort(function(a, b) {
-          if(a.seats < b.seats) return -1;
-          else if(a.seats == b.seats) return 0;
+          if (a.seats < b.seats) return -1;
+          else if (a.seats == b.seats) return 0;
         });
 
         return [[suitableTables[0]]];
       }
 
-      
       var availableTables = [];
-      for(t of this.allTables) {
-        if(t.isFree && !t.isDisabled) availableTables.push(t);
+      for (t of this.allTables) {
+        if (t.isFree && !t.isDisabled) availableTables.push(t);
       }
 
-      for(var t1 of this.allTables) {
-        for(var t2 of this.allTables) {
-          if(t1.id == t2.id) continue;
+      for (var t1 of this.allTables) {
+        for (var t2 of this.allTables) {
+          if (t1.id == t2.id) continue;
+          for (var t3 of this.allTables) {
+            if (t3.id == t2.id || t3.id == t1.id) {
+              if (!t1.isDisabled && !t2.isDisabled && !t3.isDisabled) {
+                var sumSeats = parseInt(t1.seats) + parseInt(t2.seats) + parseInt(t3.seats);
+                if (sumSeats > this.numberOfSeats) {
+                  var maximumNumberOfSeats = parseInt(this.numberOfSeats + 3);
+                  if (maximumNumberOfSeats > sumSeats) {
+                    suitableTables.push[(t1, t2, t3)];
 
-          for(var t3 of this.allTables) {
-            if(t3.id == t2.id || t3.id == t1.id) continue;
-
-            if(t1.seats + t2.seats + t3.seats >= this.numberOfSeats) suitableTables.push([t1, t2, t3]);
+                    console.log(sumSeats + ' Summe');
+                    console.log(this.numberOfSeats + ' max');
+                    console.log(suitableTables)
+                  }
+                }
+              }
+            }
           }
+
+          // if(t1.seats + t2.seats + t3.seats >= ssthis.numberOfSeats
+
+          // && t1.seats + t2.seats + t3.seats < this.numberOfSeats + 4 && !t1.isDisabled && !t2.isDisabled && !t3.isDisabled) suitableTables.push([t1, t2, t3]);
         }
       }
-
       return suitableTables;
     },
-    
-    // numberOfSeats() {
-    //     return this.$store.getters.StepOne.numberOfSeats;
-    // },
+
+    // return suitableTables;
   },
+
+  // numberOfSeats() {
+  //     return this.$store.getters.StepOne.numberOfSeats;
+  // },
 };
 </script>
 
@@ -143,7 +152,7 @@ export default {
   float: right;
 }
 
-ul{
+ul {
   list-style: none !important;
 }
 </style>
